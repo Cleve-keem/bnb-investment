ffe2679e48beb2e09911b4420dbccff8d007a811
cleve-keem <belloharyourmidey@gmail.com>
2026-08-25 17:41:38 +0100

    fix: netlify register route not a module

--- app/api/v1/auth/register/route.ts
+++ app/api/v1/auth/register/route.ts
@@ -1,28 +1,28 @@
-// import { AuthService } from "@/libs/auth/auth.service";
-// import { ApiResponse } from "@/libs/http/api-response";
-// import { createClient } from "@/libs/supabase/server";
-// import { RegisterUseCase } from "@/libs/application/register.use-case";
-// import { ErrorHandler } from "@/libs/errors";
-// import { registerSchema } from "@/libs/validations/auth";
-// import { cookies } from "next/headers";
+import { AuthService } from "@/libs/auth/auth.service";
+import { ApiResponse } from "@/libs/http/api-response";
+import { createClient } from "@/libs/supabase/server";
+import { RegisterUseCase } from "@/libs/application/register.use-case";
+import { ErrorHandler } from "@/libs/errors";
+import { registerSchema } from "@/libs/validations/auth";
+import { cookies } from "next/headers";
 
-// export async function POST(request: Request) {
-//   try {
-//     const body = await request.json();
-//     const credentials = registerSchema.parse(body);
+export async function POST(request: Request) {
+  try {
+    const body = await request.json();
+    const credentials = registerSchema.parse(body);
 
-//     const cookieStore = await cookies();
-//     const supabase = createClient(cookieStore);
+    const cookieStore = await cookies();
+    const supabase = createClient(cookieStore);
 
-//     const useCase = new RegisterUseCase(new AuthService(supabase));
-//     const user = await useCase.execute(credentials);
+    const useCase = new RegisterUseCase(new AuthService(supabase));
+    const user = await useCase.execute(credentials);
 
-//     return ApiResponse.created(
-//       user,
-//       "Registration successful. Please check your email to verify your account.",
-//     );
-//   } catch (error: any) {
-//     console.log("api error message", error.message);
-//     return ErrorHandler.handle(error);
-//   }
-// }
+    return ApiResponse.created(
+      user,
+      "Registration successful. Please check your email to verify your account.",
+    );
+  } catch (error: any) {
+    console.log("api error message", error.message);
+    return ErrorHandler.handle(error);
+  }
+}
